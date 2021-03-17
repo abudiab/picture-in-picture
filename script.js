506b01f66f4106f583d2aa1803a323e425e49e67
Mahmoud <mahmoud.w.diab@gmail.com>
2021-03-17 16:20:23 +0200

    now getting media stream and pushing that to the video object

---
+++ script.js
@@ -0,0 +1,16 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Prompt to select a media stream, pass to video element, then play
+
+const selectMediaStream = async () => {
+  try {
+    meh;
+  } catch (error) {
+    // Catch errors
+    console.log("Opsy dops, something isn't right..", error);
+  }
+};
+
+// On Load
+selectMediaStream();
